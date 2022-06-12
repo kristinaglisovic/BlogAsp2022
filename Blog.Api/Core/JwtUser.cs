b63671f3e3595581b327f69a0d3c391fa09bcfbf
KimixPhotography <58527805+KimixPhotography@users.noreply.github.com>
2022-06-12 12:18:19 +0200

    initial commit

---
+++ Blog.Api/Core/JwtUser.cs
@@ -0,0 +1,25 @@
+﻿using Blog.Domain.Entities;
+using System.Collections.Generic;
+
+namespace Blog.Api.Core
+{
+    public class JwtUser : IApplicationUser
+    {
+        public string Identity { get; set;}
+
+        public int Id { get; set; }
+        public IEnumerable<int> UseCaseIds { get; set; } = new List<int>();
+        public string Email { get; set; }
+    }
+
+    public class AnonimousUser : IApplicationUser
+    {
+        public string Identity => "Anonymous";
+
+        public int Id => 0;
+
+        public IEnumerable<int> UseCaseIds => new List<int> { 1, 2, 4, 6, 22, 24, 33, 35, 42, 43, 44, 18, 19 };
+
+        public string Email => "anonimous@blog-asp-api.com";
+    }
+}
---
+++ Blog.Application/UseCases/DTO/User/UserDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Application.UseCases.DTO.User
+{
+    public class UserDTO : BaseDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+
+        public int PostsCount { get; set; }
+        public int LikesCount { get; set; }
+        public int CommentsCount { get; set; }
+        public bool IsActive { get; set; }
+
+        public string Image { get; set; }
+    }
+
+       
+}
